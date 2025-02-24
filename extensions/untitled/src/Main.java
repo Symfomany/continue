47ef9b4108429f8a6155acb8a28fe11c21e9fc04
Julien Taiwa <taiwa.julien@gmail.com>
2025-02-24 09:47:32 +0100

    add changes

--- extensions/untitled/src/Main.java
+++ extensions/untitled/src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int age = 35;
+        String name = "John Doe";
+        double height = 1.75;
+        boolean isStudent = true;
+        char gender = 'M';
+
+
 
 
 
